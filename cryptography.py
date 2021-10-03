ba07324f64d025fcbc601423d87d1ccaed521a97
Satish2021N <satishdevilliers17@gmail.com>
2021-10-03 13:26:26 +0545

    Fixed the error by second method

--- cryptography.py
+++ cryptography.py
@@ -11,9 +11,7 @@ string_output = ""
 for i in range(string_len):
     character = string_input[i]
     location_of_character = alphabets.find(character)
-    new_location_of_character = location_of_character + shift_number
-    if new_location_of_character >=26 :
-        new_location_of_character -= 26
+    new_location_of_character = (location_of_character + shift_number)%26
     string_output += alphabets[new_location_of_character]
 
 print("Encryted: ", string_output)
