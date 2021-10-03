d6264fa8b01b9ad91f47c99742ca45d06d641b45
Satish2021N <satishdevilliers17@gmail.com>
2021-10-03 13:07:04 +0545

    Added user shift number

--- cryptography.py
+++ cryptography.py
@@ -2,6 +2,7 @@ alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 string_input = input("Enter a word: ")
 
+shift_number = int(input("Enter a number to shift  by: "))
 
 string_len = len(string_input)
 
@@ -10,7 +11,7 @@ string_output = ""
 for i in range(string_len):
     character = string_input[i]
     location_of_character = alphabets.find(character)
-    new_location_of_character = location_of_character + 3
+    new_location_of_character = location_of_character + shift_number
     string_output += alphabets[new_location_of_character]
 
 print("Encryted: ", string_output)
